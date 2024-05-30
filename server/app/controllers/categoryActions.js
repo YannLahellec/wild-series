3ae63074339212044c1b32bf6494427fc9759d56
YannLahellec <lahellec.yann@gmail.com>
2024-05-30 11:30:18 +0200

    quete 6 done

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,3 +1,4 @@
+const tables = require("../../database/tables")
 // Some data to make the trick
 
 const categories = [
@@ -15,16 +16,10 @@ const categories = [
 
   // Declare the actions
   
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((categorie) =>
-      categorie.synopsis.includes(req.query.q)
-    );
+const browse = async (req, res) => {
+const categoriesFromDB = await tables.category.readAll();
 
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
+res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -1,3 +1,4 @@
+const tables = require("../../database/tables")
 // Some data to make the trick
 
 const programs = [
@@ -25,16 +26,9 @@ const programs = [
 
 // Declare the action
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q)
-    );
-
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+const browse = async (req, res) => {
+const programsFromDB = await tables.program.readAll();
+res.json(programsFromDB)
 };
 
 const read = (req, res) => {
